0450efb40688cb61106d41126845c936376e0a0d
npham140201 <npham140201@gmail.com>
2025-11-11 21:14:44 +0700

    update 11/11/2025 change folder and custom  size box

---
+++ app/src/options/OptionSumary.svelte
@@ -0,0 +1,55 @@
+<script>
+</script>
+
+<div
+  class="flex flex-col bg-white rounded-lg shadow-md border border-gray-200 overflow-hidden"
+  style="height: calc(100vh - 130px); min-height: 300px;"
+>
+  <div
+    class="w-full h-[50px] flex flex-row border-b border-gray-200 items-center"
+  >
+    <div class="flex-1 text-center text-[14px] font-bold text-lg">
+      Hãy chọn chủ đề bạn quan tâm?
+    </div>
+  </div>
+  <div class="flex-1 overflow-y-auto p-4">
+    <ul class="space-y-4">
+      <li
+        class="flex flex-row border-b border-gray-300 py-2 hover:bg-gray-50 cursor-pointer"
+      >
+        <div
+          class="w-[28px] h-[28px] bg-[green] text-white flex justify-center items-center rounded-[4px]"
+        >
+          T
+        </div>
+        <h2 class="text-[12.25px] ml-2 flex items-center font-semibold">
+          Tư vấn cây trồng
+        </h2>
+      </li>
+    </ul>
+    <li
+      class="flex flex-row border-b border-gray-300 py-2 hover:bg-gray-50 cursor-pointer"
+    >
+      <div
+        class="w-[28px] h-[28px] bg-[green] text-white flex justify-center items-center rounded-[4px]"
+      >
+        T
+      </div>
+      <h2 class="text-[12.25px] ml-2 flex items-center font-semibold">
+        Tư vấn dịch vụ
+      </h2>
+    </li>
+    <li
+      class="flex flex-row border-b border-gray-300 py-2 hover:bg-gray-50 cursor-pointer"
+    >
+      <div
+        class="w-[28px] h-[28px] bg-[green] text-white flex justify-center items-center rounded-[4px]"
+      >
+        L
+      </div>
+      <h2 class="text-[12.25px] ml-2 flex items-center font-semibold">
+        Liên Hệ
+      </h2>
+    </li>
+  </div>
+</div>
